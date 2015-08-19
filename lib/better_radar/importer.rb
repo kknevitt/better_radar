61c903c5c5e576d26a5d5fefbbdd3150c3209e97
kknevitt <knev123@hotmail.com>
2015-08-19 12:04:29 +0100

    building url and using key and username provided

--- lib/better_radar/importer.rb
+++ lib/better_radar/importer.rb
@@ -1,14 +1,41 @@
+require 'nokogiri'
+
 class Importer
 
   URL = "https://www.betradar.com/betradar/getXmlFeed.php"
-  PARAMS =  {
-              bookmakerName: "UserName",
-              xmlFeedName: "Fixtures",
-              deleteAfterTransfer: "no",
-              key: "123456"
-            }
 
-  def self.get_feed
-    @xml_string = RestClient.get URL, :params => PARAMS
+  attr_accessor :feed_name, :user, :key, :options
+
+  def initialize(feed_name, bookmaker, key, options = {})
+    self.feed_name = feed_name
+    self.user = bookmaker
+    self.key = key
+
+    self.options = options
   end
+
+  def get_feed
+    @xml_data = Nokogiri::XML(open(build_url))
+  end
+
+  private
+
+  def build_url
+    generated_url = URL << "?"
+    feed_arguments.each_with_index do |(key, value), index|
+      generated_url << "#{key}=#{value}"
+      generated_url << "&" unless index == feed_arguments.length - 1
+    end
+    generated_url
+  end
+
+  def feed_arguments
+    args = {
+      bookmakerName: self.user,
+      xmlFeedName: self.feed_name,
+      key: self.key
+    }
+    args.merge!(options)
+  end
+
 end
